87dbbb91fec94a60d16b2ad16bb169e7157093e2
trannhatnam2707 <tran.nam27072003@gmail.com>
2025-11-18 17:08:55 +0700

    change banner img finish

--- src/pages/index/banner.tsx
+++ src/pages/index/banner.tsx
@@ -1,10 +1,19 @@
 import React, { FC } from "react";
 import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { getDummyImage } from "utils/product";
 import { Box } from "zmp-ui";
 
+// Import trực tiếp các ảnh từ thư mục docs/dummy
+import banner1 from "../../../docs/dummy/banner-1.webp";
+import banner2 from "../../../docs/dummy/banner-2.webp";
+import banner3 from "../../../docs/dummy/banner-3.webp";
+import banner4 from "../../../docs/dummy/banner-4.webp";
+import banner5 from "../../../docs/dummy/banner-5.webp";
+
 export const Banner: FC = () => {
+  // Tạo mảng chứa các biến ảnh đã import
+  const banners = [banner1, banner2, banner3, banner4, banner5];
+
   return (
     <Box className="bg-white" pb={4}>
       <Swiper
@@ -16,9 +25,7 @@ export const Banner: FC = () => {
         loop
         cssMode
       >
-        {[1, 2, 3, 4, 5]
-          .map((i) => getDummyImage(`banner-${i}.webp`))
-          .map((banner, i) => (
+        {banners.map((banner, i) => (
             <SwiperSlide key={i} className="px-4">
               <Box
                 className="w-full rounded-lg aspect-[2/1] bg-cover bg-center bg-skeleton"
